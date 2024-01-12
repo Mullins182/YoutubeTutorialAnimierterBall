9f0aa0f2f7fde4d77bd50012b59ba33d14d5ec3e
Mullins182 <Mullins182@t-online.de>
2024-01-12 13:02:14 +0100

    Added two more animated balls !

--- YoutubeTutorialAnimierterBall/MainWindow.xaml.cs
+++ YoutubeTutorialAnimierterBall/MainWindow.xaml.cs
@@ -18,8 +18,13 @@ namespace YoutubeTutorialAnimierterBall
     {
         private DispatcherTimer _animationTimer = new DispatcherTimer();
 
-        private bool goesRight  = true;
-        private bool goesDown   = true;
+        private bool ball1goesRight = true;
+        private bool ball2goesRight = true;
+        private bool ball3goesRight = true;
+
+        private bool ball1goesDown = true;
+        private bool ball2goesDown = true;
+        private bool ball3goesDown = true;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,43 +35,122 @@ namespace YoutubeTutorialAnimierterBall
 
         private void positioniereBall(object? sender, EventArgs e)
         {
-            var x = Canvas.GetLeft(Ball);
-            var y = Canvas.GetTop(Ball);
+            var ball1X = Canvas.GetLeft(Ball1);
+            var ball2X = Canvas.GetLeft(Ball2);
+            var ball3X = Canvas.GetLeft(Ball3);
+            var ball1Y = Canvas.GetTop(Ball1);
+            var ball2Y = Canvas.GetTop(Ball2);
+            var ball3Y = Canvas.GetTop(Ball3);
 
-            if (x >= AnimationField.ActualWidth - Ball.ActualWidth)         // Ball im Canvas auf der X-Achse (links/rechts) bewegen !
+            if (ball1X >= AnimationField.ActualWidth - Ball1.ActualWidth)         // Ball im Canvas auf der X-Achse (links/rechts) bewegen !
             {
-                goesRight = false;
+                ball1goesRight = false;
             }
-            else if (x <= 0)
+            else if (ball1X <= 0)
             {
-                goesRight = true;
+                ball1goesRight = true;
+            }
+            
+            if (ball2X >= AnimationField.ActualWidth - Ball2.ActualWidth)
+            {
+                ball2goesRight = false;
+            }
+            else if (ball2X <= 0)
+            {
+                ball2goesRight = true;
             }
 
-            if (goesRight)
+            if (ball3X >= AnimationField.ActualWidth - Ball3.ActualWidth)
             {
-                Canvas.SetLeft(Ball, x + 2);                
+                ball3goesRight = false;
+            }
+            else if (ball3X <= 0)
+            {
+                ball3goesRight = true;
+            }
+
+
+            if (ball1goesRight)
+            {
+                Canvas.SetLeft(Ball1, ball1X + 2);                
             }
             else
             {
-                Canvas.SetLeft(Ball, x - 2);
+                Canvas.SetLeft(Ball1, ball1X - 2);
             }
 
-            if (y >= AnimationField.ActualHeight - Ball.ActualHeight)       // Ball im Canvas auf der Y-Achse (auf/ab) bewegen !
+            if (ball2goesRight)
             {
-                goesDown = false;
-            }
-            else if (y <= 0)
-            {
-                goesDown = true;
-            }
-
-            if (goesDown)
-            {
-                Canvas.SetTop(Ball, y + 2);
+                Canvas.SetLeft(Ball2, ball2X + 2);
             }
             else
             {
-                Canvas.SetTop(Ball, y - 2);
+                Canvas.SetLeft(Ball2, ball2X - 2);
+            }
+
+            if (ball3goesRight)
+            {
+                Canvas.SetLeft(Ball3, ball3X + 2);
+            }
+            else
+            {
+                Canvas.SetLeft(Ball3, ball3X - 2);
+            }
+
+
+            if (ball1Y >= AnimationField.ActualHeight - Ball1.ActualHeight)       // Ball im Canvas auf der Y-Achse (auf/ab) bewegen !
+            {
+                ball1goesDown = false;
+            }
+            else if (ball1Y <= 0)
+            {
+                ball1goesDown = true;
+            }
+
+            if (ball2Y >= AnimationField.ActualHeight - Ball2.ActualHeight)       // Ball im Canvas auf der Y-Achse (auf/ab) bewegen !
+            {
+                ball2goesDown = false;
+            }
+            else if (ball2Y <= 0)
+            {
+                ball2goesDown = true;
+            }
+
+            if (ball3Y >= AnimationField.ActualHeight - Ball3.ActualHeight)       // Ball im Canvas auf der Y-Achse (auf/ab) bewegen !
+            {
+                ball3goesDown = false;
+            }
+            else if (ball3Y <= 0)
+            {
+                ball3goesDown = true;
+            }
+
+
+            if (ball1goesDown)
+            {
+                Canvas.SetTop(Ball1, ball1Y + 2);
+            }
+            else
+            {
+                Canvas.SetTop(Ball1, ball1Y - 2);
+            }
+
+            if (ball2goesDown)
+            {
+                Canvas.SetTop(Ball2, ball2Y + 2);
+            }
+            else
+            {
+                Canvas.SetTop(Ball2, ball2Y - 2);
+            }
+
+            if (ball3goesDown)
+            {
+                Canvas.SetTop(Ball3, ball3Y + 2);
+            }
+            else
+            {
+                Canvas.SetTop(Ball3, ball3Y - 2);
             }
         }
 
