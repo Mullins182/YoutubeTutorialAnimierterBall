acd0281eebd5aca3e982f1d7e8d99235cc14e42a
Mullins182 <Mullins182@t-online.de>
2024-01-16 20:55:48 +0100

    frontend editings.

--- YoutubeTutorialAnimierterBall/MainWindow.xaml.cs
+++ YoutubeTutorialAnimierterBall/MainWindow.xaml.cs
@@ -120,7 +120,7 @@ namespace YoutubeTutorialAnimierterBall
             var dist_b1_b3 = Math.Sqrt(Math.Pow(ball1_M - ball3_M, 2) + Math.Pow(ball1yM - ball3yM, 2));
             var dist_b2_b3 = Math.Sqrt(Math.Pow(ball2_M - ball3_M, 2) + Math.Pow(ball2yM - ball3yM, 2));
 
-            PositionInfo.Content = $"Dist B1 > B2: { Math.Round(dist_b1_b2, 3)} \n\nDist B1 > B3: {Math.Round(dist_b1_b3, 3)} \n\nDist B2 > B3: {Math.Round(dist_b2_b3, 3)} \n\nBall 1 Y-Pos: {((Int16)ball1Y)} \n\nBall 2 Y-Pos: {((Int16)ball2Y)} \n\nBall 3 Y-Pos: {((Int16)ball3Y)}";
+            PositionInfo.Content = $"Dist B1 to B2: { Math.Round(dist_b1_b2, 3)} \n\nDist B1 to B3: {Math.Round(dist_b1_b3, 3)} \n\nDist B2 to B3: {Math.Round(dist_b2_b3, 3)} \n\nBall Collisions: {collision_counter / 2} \n\nSound 5 Played: {sound5_played} \n\nSound Counter: {collision_sound_counter / 2}";
 
             // Kollisionslogik Ball-Kollisionen (X-Achse)                               I N  A R B E I T !!!
 
